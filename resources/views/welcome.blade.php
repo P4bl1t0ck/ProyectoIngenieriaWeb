d11e66dfe6f840de0068f03df381dee1d77cc313
Pablo.Montalvo <pablo.montalvo.marroquin@udla.edu.ec>
2026-05-21 17:26:07 -0500

    Leccion 7 agregando CSS & Tailwind

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ninja Network</title>
+
+
+    @vite('resources/css/app.css')
 </head>
 <body>
     <h1>Welcome TO ninja.com</h1>
@@ -12,5 +15,6 @@
     <a href="/ninjas" class="btn">
         Find Ninjas!
     </a>
+    
 </body>
 </html>
